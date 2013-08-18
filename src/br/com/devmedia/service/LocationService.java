6819b9020333a0f71189a1cba4a08ec910506199
Mateus Freira <mateus.freira@gmail.com>
2013-08-18 12:59:46 -0300

    Adicionando verifyGPSEnabled

--- src/br/com/devmedia/service/LocationService.java
+++ src/br/com/devmedia/service/LocationService.java
@@ -1,6 +1,9 @@
 package br.com.devmedia.service;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -55,6 +58,39 @@ public class LocationService {
 
 	public void stop() {
 		locationManager.removeUpdates(listener);
+	}
+
+	public boolean verifyGPSEnabled(boolean showAlert) {
+		boolean result = locationManager
+				.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		if (!result && showAlert) {
+			sugestGPSEnabled();
+		}
+		return result;
+	}
+
+	public void sugestGPSEnabled() {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setMessage(
+				"Opa percebemos que seu gps esta dasabilitado, deseja habilitar agora?")
+				.setCancelable(false)
+				.setPositiveButton("SIm",
+						new DialogInterface.OnClickListener() {
+							public void onClick(final DialogInterface dialog,
+									final int id) {
+								context.startActivity(new Intent(
+										android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+							}
+						})
+				.setNegativeButton("Não",
+						new DialogInterface.OnClickListener() {
+							public void onClick(final DialogInterface dialog,
+									final int id) {
+								dialog.cancel();
+							}
+						});
+		final AlertDialog alert = builder.create();
+		alert.show();
 	}
 
 }
