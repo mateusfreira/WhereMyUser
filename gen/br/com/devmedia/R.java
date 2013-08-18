c75378ff9d99ee9ea84e6e67175fcd63cd7ad8f6
Mateus Freira <mateus.freira@gmail.com>
2013-08-18 13:16:08 -0300

    Finalizando a UI

--- gen/br/com/devmedia/R.java
+++ gen/br/com/devmedia/R.java
@@ -24,9 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button1=0x7f080001;
+        public static final int action_settings=0x7f080006;
         public static final int current_location=0x7f080000;
+        public static final int last_gps=0x7f080001;
+        public static final int last_network=0x7f080002;
+        public static final int update_gps=0x7f080003;
+        public static final int update_network=0x7f080004;
+        public static final int update_passive=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
