6c2b044c53b9963ebdf878a5e775a37903a45ed3
Mateus Freira <mateus.freira@gmail.com>
2013-08-18 13:26:53 -0300

    Adicionando locationService na activity

--- src/br/com/devmedia/MainActivity.java
+++ src/br/com/devmedia/MainActivity.java
@@ -1,22 +1,51 @@
 package br.com.devmedia;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
 import android.view.Menu;
+import br.com.devmedia.service.LocationService;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
+
+	private LocationService locationService;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.locationService = new LocationService(this, this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
